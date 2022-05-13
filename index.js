aab2a7daa7debd85281605e12e31c882c689c62a
Sakir Hossain Faruque <84528217+sakir104@users.noreply.github.com>
2022-05-13 11:58:23 +0600

    c

---
+++ index.js
@@ -0,0 +1,4 @@
+import {text,mes} from './mumodule.js'
+console.log(text);
+mes("Goodbye everone")
+console.log(text);
---
+++ mumodule.js
@@ -0,0 +1,5 @@
+export let text = "It is text";
+
+export function mes(txt){
+    text = txt;
+}
